b0776b2c8ee544f61a95b881cbd04277e295bb98
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2024-07-29 20:22:01 +0500

    first commit

---
+++ twitter_clone_backend/database.py
@@ -0,0 +1,41 @@
+from sqlmodel import SQLModel, Field, create_engine, Relationship
+from typing import Optional, List
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("conn_str").replace("postgres://", "postgresql+psycopg2://")
+
+class Comment(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    tweet_id: Optional[int] = Field(default=None, foreign_key="tweet.id")
+    profile: str
+    username: str
+    content: str
+    tweet: 'Tweet' = Relationship(back_populates="comments")
+
+class UserLike(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    tweet_id: Optional[int] = Field(default=None, foreign_key="tweet.id")
+    e_mail: str
+    pass_word: str
+    tweet: 'Tweet' = Relationship(back_populates="like_user_ids")
+
+class Tweet(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    profile: str
+    content: str
+    slug: str
+    contentImage: Optional[str] = Field(default=None, alias="contentImage")
+    likesNumber: int = Field(default=0, alias="likesNumber")
+    password: str
+    email: str
+    commentsNumber: int
+    comments: List[Comment] = Relationship(back_populates="tweet")
+    likeUserIds: List[UserLike] = Relationship(back_populates="tweet")
+
+if __name__ == "__main__":
+    engine = create_engine(DATABASE_URL)
+    SQLModel.metadata.create_all(engine)
